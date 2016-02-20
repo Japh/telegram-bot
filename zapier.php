f063649917cea9df21464ebc734644989c7b080a
Japh <japhie@gmail.com>
2016-02-21 02:37:24 +1100

    Clean up zapier

--- zapier.php
+++ zapier.php
@@ -1,9 +1,8 @@
 <?php
-    $json = file_get_contents('php://input');
-    if (!$json) {
-        return;
-    }
-    $data = (array) json_decode($json, TRUE);
-    telegram_log('------>', 'ZAPIER', json_encode((array) file_get_contents("php://input")));
-    telegram_sendmessagetoall($data['hook']);
-?>
+$json = file_get_contents( 'php://input' );
+if ( ! $json ) {
+    return;
+}
+$data = ( array ) json_decode( $json, true );
+telegram_log( '------>', 'ZAPIER', json_encode( ( array ) file_get_contents( "php://input" ) ) );
+telegram_sendmessagetoall( $data['hook'] );
